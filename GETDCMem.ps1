1a2fcfb7141e02e8b5e450f37ad859b8a045b451
andshill7 <andrewdeanhill@hotmail.com>
2022-02-09 15:13:46 +0000

    Add files via upload

---
+++ GETDCMem.ps1
@@ -0,0 +1,23 @@
+﻿
+$x = Get-Content -Path c:\temp\dcs.txt 
+
+ foreach ($DC in $x)
+
+ {
+
+$colItems = get-wmiobject -class "Win32_ComputerSystem" -namespace "root\CIMV2" -computername $DC
+
+foreach ($objItem in $colItems){
+$displayGB = [math]::round($objItem.TotalPhysicalMemory/1024/1024/1024, 0)
+write-host "Total Physical Memory: " $displayGB "GB"
+write-host "Model: " $objItem.Model
+}
+
+$colItems = get-wmiobject -class "Win32_Processor" -namespace "root\CIMV2" -computername $DC
+foreach ($objItem in $colItems){
+write-host "System Name: " $objItem.SystemName
+
+
+}
+
+}
